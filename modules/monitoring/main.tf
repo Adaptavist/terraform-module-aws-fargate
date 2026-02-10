392f48443d7fb5e272990b6d1effcbbae5ecb4a5
Romy Greenfield <rgreenfield@adaptavist.com>
2026-02-10 11:18:14 +0000

    fix: only have cpu utilisation high in prod environment

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -227,6 +227,8 @@ resource "aws_cloudwatch_metric_alarm" "unhealthy_host_count" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "cpu_utilisation_high" {
+  count               = var.env == "Prod" ? 1 : 0
+
   alarm_name          = "${var.fargate_service_name}-cpu-utilisation-high"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 2
